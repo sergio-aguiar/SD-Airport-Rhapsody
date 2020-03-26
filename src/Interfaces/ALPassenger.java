0aa4fd247e0a4c4d92a92f0bb374e591a851126d
pioavenger <sergio.aguiar@ua.pt>
2020-03-26 18:45:48 +0000

    Changed AL to have data on the shared region side.

--- src/Interfaces/ALPassenger.java
+++ src/Interfaces/ALPassenger.java
@@ -3,6 +3,6 @@ package Interfaces;
 import Entities.PassengerThread;
 
 public interface ALPassenger {
-    public String whatShouldIDo(int pid);
+    public void whatShouldIDo(int pid);
     public void goCollectABag(int pid);
 }
--- src/SharedRegions/ArrivalLounge.java
+++ src/SharedRegions/ArrivalLounge.java
@@ -2,9 +2,11 @@ package SharedRegions;
 
 import Entities.PassengerThread;
 import Entities.PorterThread;
+import Extras.Bag;
 import Interfaces.ALPassenger;
 import Interfaces.ALPorter;
 
+import java.util.Stack;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -13,15 +15,42 @@ public class ArrivalLounge implements ALPassenger, ALPorter {
     private final ReentrantLock reentrantLock;
     private final Condition porterCondition;
 
-    private int passengersThatReached;
+    private final int totalPassengers;
+    private int passengersThatArrived;
+
+    private int flightNumber;
+
+    private final int[] luggageNumberPerFlight;
+    private final Bag[][] luggagePerFlight;
+    private Stack<Bag> bagsInThePlane;
+    private int luggagePickedUp;
 
     private final Repository repository;
 
-    public ArrivalLounge(Repository repository) {
+    public ArrivalLounge(Repository repository, int totalPassengers, int[] luggageNumberPerFlight,
+                         Bag[][] luggagePerFlight) {
         this.reentrantLock = new ReentrantLock();
         this.porterCondition = this.reentrantLock.newCondition();
-        this.passengersThatReached = 0;
+        this.totalPassengers = totalPassengers;
+        this.passengersThatArrived = 0;
+        this.flightNumber = 0;
+        this.luggageNumberPerFlight = luggageNumberPerFlight;
+        this.luggagePerFlight = luggagePerFlight;
+        this.bagsInThePlane = new Stack<>();
+        this.bagArrayToStack(0);
+        this.luggagePickedUp = 0;
         this.repository = repository;
+    }
+
+    private void bagArrayToStack(int flightNumber) {
+        for(int i = 0; i < this.luggagePerFlight[this.flightNumber].length; i++)
+            if(this.luggagePerFlight[this.flightNumber][i] != null)
+                bagsInThePlane.push(this.luggagePerFlight[this.flightNumber][i]);
+    }
+
+    public void nextFlight() {
+        this.flightNumber++;
+        this.bagArrayToStack(this.flightNumber);
     }
 
     @Override
@@ -29,8 +58,7 @@ public class ArrivalLounge implements ALPassenger, ALPorter {
         boolean done = false;
         this.reentrantLock.lock();
         try {
-            if(this.repository.isPorterDone()) done = true;
-            else this.porterCondition.await();
+            this.porterCondition.await();
         } catch (Exception e) {
             System.out.print(e.toString());
         } finally {
@@ -40,16 +68,16 @@ public class ArrivalLounge implements ALPassenger, ALPorter {
     }
 
     @Override
-    public String whatShouldIDo(int pid) {
-        this.passengersThatReached++;
-        if(this.passengersThatReached == this.repository.getNumberOfPassengers()) this.porterCondition.signal();
-        return this.repository.getPassengerSituation(pid).toString();
+    public void whatShouldIDo(int pid) {
+        this.passengersThatArrived++;
+        if(this.passengersThatArrived == this.totalPassengers) this.porterCondition.signal();
     }
 
     @Override
     public String tryToCollectABag(int pid) {
         this.repository.setPorterState(pid, PorterThread.PorterStates.AT_THE_PLANES_HOLD);
-        if(this.repository.getNumberOfLuggageLeftToCollect() != 0) return this.repository.getBag();
+        if(this.luggageNumberPerFlight[this.flightNumber] != this.luggagePickedUp)
+            return this.bagsInThePlane.pop().getBagSituation().toString();
         return null;
     }
 
